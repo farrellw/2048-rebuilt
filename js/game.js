74a30b5be076b711035de51dd142d2500e981731
Will Farrell & Henry Fyfe <pair+farrellw.hfyfe@devbootcamp.com>
2016-10-31 12:32:32 -0500

    Add semicolons

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 var Game = function(gameString = '') {
   this.gameString = gameString;
   if ( gameString.length === 0) {
-    var randOne = Math.floor(Math.random() * 16)
-    var randTwo = Math.floor(Math.random() * 16)
+    var randOne = Math.floor(Math.random() * 16);
+    var randTwo = Math.floor(Math.random() * 16);
     for ( var i = 0; i < 16; i++) {
       if (i === randOne || i === randTwo) {
        this.gameString += 2;
@@ -19,9 +19,9 @@ Game.prototype.toString = function() {
   for ( var i = 0 ; i < 4 ; i++ ) {
     output += this.gameString.substr ( 4*i , 4 );
     output += '\n' ;
-  } ;
+  };
   return output ;
-} ;
+};
 
 Game.prototype.move = function(direction) {
   switch (direction)
@@ -29,78 +29,78 @@ Game.prototype.move = function(direction) {
     case "up":
       console.log("Going up");
       for ( var i = 4; i < 16; i++) {
-        var neighbor = this.gameString[i-4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i-4];
+        var current = this.gameString[i];
         if (neighbor === "0" && current != "0") {
           this.gameString = this.gameString.replaceAt(i-4, this.gameString[i]);
             this.gameString = this.gameString.replaceAt(i, "0");
-            i -= 5
-        }
-      }
+            i -= 5;
+        };
+      };
       for ( var i = 4; i < 16; i++) {
-        var neighbor = this.gameString[i-4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i-4];
+        var current = this.gameString[i];
         if (current != "0" && current === neighbor) {
           this.gameString = this.gameString.replaceAt(i-4, String(current * 2) );
           this.gameString = this.gameString.replaceAt(i, "0");
-        }
-      }
+        };
+      };
       for ( var i = 4; i < 16; i++) {
-        var neighbor = this.gameString[i-4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i-4];
+        var current = this.gameString[i];
         if (neighbor === "0" && current != "0") {
           this.gameString = this.gameString.replaceAt(i-4, this.gameString[i]);
           this.gameString = this.gameString.replaceAt(i, "0");
-          i -= 5
-        }
-      }
+          i -= 5;
+        };
+      };
       break;
     case "down":
       console.log("Going down");
       for ( var i = 11; i > -1; i--) {
-        var neighbor = this.gameString[i+4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i+4];
+        var current = this.gameString[i];
         if (neighbor === "0" && current != "0") {
           this.gameString = this.gameString.replaceAt(i+4, this.gameString[i]);
           this.gameString = this.gameString.replaceAt(i, "0");
-          i += 5
-        }
-      }
+          i += 5;
+        };
+      };
       for ( var i = 11; i > -1; i--) {
-        var neighbor = this.gameString[i+4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i+4];
+        var current = this.gameString[i];
         if (current != "0" && current === neighbor) {
           this.gameString = this.gameString.replaceAt(i+4, String(current * 2) );
           this.gameString = this.gameString.replaceAt(i, "0");
-        }
-      }
+        };
+      };
       for ( var i = 11; i > -1; i--) {
-        var neighbor = this.gameString[i+4]
-        var current = this.gameString[i]
+        var neighbor = this.gameString[i+4];
+        var current = this.gameString[i];
         if (neighbor === "0" && current != "0") {
           this.gameString = this.gameString.replaceAt(i+4, this.gameString[i]);
           this.gameString = this.gameString.replaceAt(i, "0");
-          i += 5
-        }
-      }
+          i += 5;
+        };
+      };
       break;
     case "left":
       console.log("Going left");
       for ( var i = 1; i < 16; i++) {
         if (i % 4 != 0 ) {
-          var neighbor = this.gameString[i-1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i-1];
+          var current = this.gameString[i];
           if (neighbor === "0" && current != "0") {
             this.gameString = this.gameString.replaceAt(i-1, this.gameString[i]);
             this.gameString = this.gameString.replaceAt(i, "0");
-            i -= 2
+            i -= 2;
           }
         }
       }
       for ( var i = 1; i < 16; i++) {
         if (i % 4 != 0 ) {
-          var neighbor = this.gameString[i-1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i-1];
+          var current = this.gameString[i];
           if (current != "0" && current === neighbor) {
             this.gameString = this.gameString.replaceAt(i-1, String(current * 2) );
             this.gameString = this.gameString.replaceAt(i, "0");
@@ -109,12 +109,12 @@ Game.prototype.move = function(direction) {
       }
       for ( var i = 1; i < 16; i++) {
         if (i % 4 != 0 ) {
-          var neighbor = this.gameString[i-1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i-1];
+          var current = this.gameString[i];
           if (neighbor === "0" && current != "0") {
             this.gameString = this.gameString.replaceAt(i-1, this.gameString[i]);
             this.gameString = this.gameString.replaceAt(i, "0");
-            i -= 2
+            i -= 2;
           }
         }
       }
@@ -123,36 +123,36 @@ Game.prototype.move = function(direction) {
       console.log("Going right");
       for ( var i = 15; i > -1; i--) {
         if (i % 4 != 3 ) {
-          var neighbor = this.gameString[i+1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i+1];
+          var current = this.gameString[i];
           if (neighbor === "0" && current != "0") {
             this.gameString = this.gameString.replaceAt(i+1, this.gameString[i]);
             this.gameString = this.gameString.replaceAt(i, "0");
-            i += 2
-          }
-        }
-      }
+            i += 2;
+          };
+        };
+      };
       for ( var i = 15; i > -1; i--) {
         if (i % 4 != 3 ) {
-          var neighbor = this.gameString[i+1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i+1];
+          var current = this.gameString[i];
           if (current != "0" && current === neighbor) {
             this.gameString = this.gameString.replaceAt(i+1, String(current * 2) );
             this.gameString = this.gameString.replaceAt(i, "0");
-          }
-        }
-      }
+          };
+        };
+      };
       for ( var i = 15; i > -1; i--) {
         if (i % 4 != 3 ) {
-          var neighbor = this.gameString[i+1]
-          var current = this.gameString[i]
+          var neighbor = this.gameString[i+1];
+          var current = this.gameString[i];
           if (neighbor === "0" && current != "0") {
             this.gameString = this.gameString.replaceAt(i+1, this.gameString[i]);
             this.gameString = this.gameString.replaceAt(i, "0");
-            i += 2
-          }
-        }
-      }
+            i += 2;
+          };
+        };
+      };
       break;
   };
 };
