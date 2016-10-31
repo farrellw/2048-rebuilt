eec7912e5564d51ae9ca6b93fa957359fd0f41f0
Will Farrell & Henry Fyfe <pair+farrellw.hfyfe@devbootcamp.com>
2016-10-31 10:09:53 -0500

    Add gameString logic to return a user entered game string or generate a board with two random numbers

--- js/game.js
+++ js/game.js
@@ -1,6 +1,18 @@
-var Game = function(gameString = '0000000000000000') {
-    this.gameString = gameString ;
-} ;
+var Game = function(gameString = '') {
+  this.gameString = gameString;
+  if ( gameString.length === 0) {
+    var randOne = Math.floor(Math.random() + 16)
+    var randTwo = Math.floor(Math.random() + 16)
+    for ( var i = 0; i < 16; i++) {
+      if (i === randOne || i === randTwo) {
+       this.gameString += 2;
+      }
+      else {
+       this.gameString += 0;
+      };
+    };
+  };
+};
 
 Game.prototype.toString = function() {
   var output = "";
